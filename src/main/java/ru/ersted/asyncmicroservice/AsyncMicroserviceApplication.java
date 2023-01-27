589eb498bbf20247b73da700dde44b71953c398c
Bogdan Rudko <rydkoc@icloud.com>
2023-01-27 08:01:29 +0300

    Initial commit

---
+++ src/main/java/ru/ersted/asyncmicroservice/AsyncMicroserviceApplication.java
@@ -0,0 +1,15 @@
+package ru.ersted.asyncmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyncMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsyncMicroserviceApplication.class, args);
+
+
+    }
+
+}
---
+++ src/test/java/ru/ersted/asyncmicroservice/AsyncMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ersted.asyncmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsyncMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
