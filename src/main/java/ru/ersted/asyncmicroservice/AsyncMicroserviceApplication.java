8a55e162d84b12171f1815b7f725edc93f36dee6
Bogdan Rudko <rydkoc@icloud.com>
2023-02-04 07:57:25 +0300

    clear main method

--- src/main/java/ru/ersted/asyncmicroservice/AsyncMicroserviceApplication.java
+++ src/main/java/ru/ersted/asyncmicroservice/AsyncMicroserviceApplication.java
@@ -1,28 +1,12 @@
 package ru.ersted.asyncmicroservice;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import ru.ersted.asyncmicroservice.api.IEXApi;
-import ru.ersted.asyncmicroservice.dto.CompanyDto;
-import ru.ersted.asyncmicroservice.dto.SymbolDto;
-
-import java.util.List;
 
 @SpringBootApplication
 public class AsyncMicroserviceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(AsyncMicroserviceApplication.class, args);
-
-        IEXApi api = new IEXApi();
-        List<SymbolDto> symbolsList = api.getSymbolsList();
-        //System.out.println(symbolsList);
-        for (SymbolDto symbol: symbolsList) {
-            System.out.println(api.getCompanyDataBySymbol(symbol.getSymbol()));
-        }
-
     }
 
 }
