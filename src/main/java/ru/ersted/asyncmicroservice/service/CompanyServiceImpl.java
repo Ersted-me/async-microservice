8c4a92eeaf39f36029e1a93cd730dfa927fa89b6
Bogdan Rudko <rydkoc@icloud.com>
2023-03-13 09:09:49 +0300

    не работает, потом удалить.

--- src/main/java/ru/ersted/asyncmicroservice/service/CompanyServiceImpl.java
+++ src/main/java/ru/ersted/asyncmicroservice/service/CompanyServiceImpl.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import ru.ersted.asyncmicroservice.client.IEXApiClient;
 import ru.ersted.asyncmicroservice.dto.StockDto;
+import ru.ersted.asyncmicroservice.dto.SymbolDto;
 import ru.ersted.asyncmicroservice.entity.StockEntity;
 import ru.ersted.asyncmicroservice.mapper.StockMapper;
 import ru.ersted.asyncmicroservice.repository.CompanyRepository;
@@ -31,7 +32,7 @@ public class CompanyServiceImpl implements CompanyService {
 
     private final EntityManager entityManager;
     //@Value("${spring.jpa.properties.hibernate.jdbc.batch_size}")
-    private int chunckSize = 10;
+    private int chunckSize = 1000;
 
 
     @Override
@@ -39,13 +40,9 @@ public class CompanyServiceImpl implements CompanyService {
 
         iexApiClient.getSymbolsList()
                 .thenApply(HttpEntity::getBody)
-                .thenApply(array -> {
-                    List<CompletableFuture<ResponseEntity<StockDto>>> futuresTask = new ArrayList<>();
-                    Arrays.stream(array)
-                            .limit(30)
-                            .forEach(symbol -> futuresTask.add(iexApiClient.getCompanyDataBySymbol(symbol.getSymbol())));
-                    return futuresTask;
-                })
+                .thenApply(array -> Arrays.stream(array)
+                        .map(e -> iexApiClient.getCompanyDataBySymbol(e.getSymbol()))
+                        .collect(Collectors.toList()))
                 .thenApply(list -> list.stream()
                         .map(re -> re.thenApply(HttpEntity::getBody))
                         .filter(Objects::nonNull)
@@ -53,16 +50,13 @@ public class CompanyServiceImpl implements CompanyService {
                         .collect(Collectors.toList())
                 )
                 .thenAccept(list -> Partition.ofSize(list, chunckSize)
-                        .forEach(e -> CompletableFuture
-                                .supplyAsync(() -> {
-                                    List<StockEntity> stockEntityList = new ArrayList<>();
-                                    e.forEach(secf -> {
-                                        secf.thenAccept(stockEntityList::add);
-                                        //System.out.println(stockEntityList.size());
-                                    });
-                                    return stockEntityList;
-                                })
-                                .thenAccept(stockEntityList -> companyRepository.saveAll(stockEntityList))));
+                        .forEach(e -> {
+                            List<StockEntity> stockEntities = e.stream()
+                                    .map(CompletableFuture::join)
+                                    .collect(Collectors.toList());
+                            companyRepository.saveAll(stockEntities);
+                        })
+                );
 
         //1. Get all companies
         //2. Using async and paral... get the data for all Stocks
